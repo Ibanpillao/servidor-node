4c0f3b4eb1bc1ba25ff1252d87471614b31c643c
Ivan <ibanpillao@gmail.com>
2022-04-25 19:40:44 +0200

    Primer commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// const http = require('http');
+const express = require('express');
+
+const app = express()
+
+let users = [
+    {
+        id: 1,
+        nombre: "Juan",
+        status: false
+    },
+    {
+        id: 2,
+        nombre: "Leire",
+        status: false
+    }
+];
+
+app.get('/', (request, response) => {
+    response.send('<h1>Servidor con express</h1>');
+});
+
+app.get('/api/users', (request, response) => {
+    response.json(users);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Servidor oyendo en el puerto ${PORT}`);
+});
