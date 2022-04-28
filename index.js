626d506eec2e5470f0760da9018e1d8609536c9f
Ivan <ibanpillao@gmail.com>
2022-04-28 10:42:07 +0200

    packageJson

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3050;
 const app = express();
 
 app.use(bodyParser.json());
@@ -14,7 +14,6 @@ const conexion = mysql.createConnection({
     password: '73e16742',
     database: 'heroku_980031004d924ce'
 });
-
 
 
 // const conexion = mysql.createConnection({
