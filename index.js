b20354180b395e83d12a019fbd92f1c1ccf39ab9
Ivan <ibanpillao@gmail.com>
2022-05-03 18:39:06 +0200

    Insertar user XXI

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ app.use(function(req, res, next) {
   });
 
 // CONEXION HEROKU
-// const conexion = mysql.createPool({
-//     host: 'us-cdbr-east-05.cleardb.net',
-//     user: 'b77f4ba431fed6',
-//     password: '73e16742',
-//     database: 'heroku_980031004d924ce'
-// });
+const conexion = mysql.createPool({
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b77f4ba431fed6',
+    password: '73e16742',
+    database: 'heroku_980031004d924ce'
+});
 
 // CONEXION LOCAL
-const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'mendimartxas'
-});
+// const conexion = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'mendimartxas'
+// });
 
 // conexion.connect();
 
