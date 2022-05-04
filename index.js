bccaf34fa3e8b72d13edc83b79578d8be42d0316
Ivan <ibanpillao@gmail.com>
2022-05-04 06:53:38 +0200

    post 2

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods","GET, POST, OPTIONS, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers: Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method");
-    res.header('content-type: application/json; charset=utf-8');
+    res.header("Access-Control-Allow-Headers: Authorization, X-API-KEY, Origin, X-Requested-With, Accept, Access-Control-Allow-Request-Method");
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
   });
