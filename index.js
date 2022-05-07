0c0aaee9e76b09a84aad07d05e1914b034b2c68d
Ivan <ibanpillao@gmail.com>
2022-05-07 06:09:16 +0200

    update consult 5

--- index.js
+++ index.js
@@ -160,13 +160,7 @@ app.post('/addMendiMartxa',(request, response) => {
 app.put('/update/:id',(request, response) => {
     const {id} = request.params;
     const {nombre, ciudad, distancia, fecha, participantes} = request.body;
-    const sql = null;
-
-    if (participantes == 0) {
-        sql = `UPDATE martxas SET ciudad = '${ciudad}', distancia = '${distancia}', fecha = '${fecha}',nombre = '${nombre}' WHERE idmartxas = ${id}`;
-    } else {
-        sql = `UPDATE martxas SET ciudad = '${ciudad}', distancia = '${distancia}', fecha = '${fecha}',nombre = '${nombre}',participantes = '${participantes}' WHERE idmartxas = ${id}`;
-    }
+    const sql = `UPDATE martxas SET ciudad = '${ciudad}', distancia = '${distancia}', fecha = '${fecha}',nombre = '${nombre}',participantes = '${participantes}' WHERE idmartxas = ${id}`;
     
     conexion.query(sql, error => {
         if (error) throw error;
