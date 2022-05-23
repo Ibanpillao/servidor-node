f1063197831de62a31ef5527a912f867e1c207df
Ivan <ibanpillao@gmail.com>
2022-05-23 11:58:28 +0200

    angular XI

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const secret = require('./settings/keys');
 const cors = require('cors');
-const conexion = require('./databases/config');
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
 
 app.use(cors());
 
@@ -47,68 +43,6 @@ app.use(require('./routes/users.route'));
  */
 app.get('/', (request, response) => {
     response.send('<h1>Bizkaiko mendimartxak</h1>');
-});
-
-// Login usuario
-/**
- * @swagger
- * /login-usuario:
- *  post:
- *    description: Login de usuario de App
- *    tags : 
- *     - Usuarios
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *              type: object
- *              $ref: '#/components/schemas/Usuario'
- *    responses:
- *      200:
- *        description: Login correcto
- *        content: 
- *          application/json:
- *            schema: 
- *              type: array
- *              items:
- *                $ref : '#/components/schemas/Usuario'
- */
- app.post('/login-usuario', (request, response) => {
-
-    const user = {
-        nombre : request.body.nombre,
-        password : request.body.password
-    }
-
-    if (user.nombre && user.password) {
-
-        const sql = `SELECT * FROM usuarios WHERE nombre = '${user.nombre}'`;
-
-        conexion.query(sql, (error, resul) => {
-            if (error) throw error;
-            if(resul.length > 0) {
-                bcrypt.compare( user.password, resul[0].password, (error, resultado) => {
-                    if (resultado) {
-
-                        const payLoad = {
-                            check : true
-                        }
-                
-                        const token = jwt.sign(payLoad, app.get('key'), {
-                            expiresIn : '7d',
-                            
-                        })
-
-                        response.json({success: true, message: 'Login correcto!', token : token});
-                        // response.send('Login correcto');
-                    } else response.json({success: false, message: 'Contraseña incorrecta!'});
-                })
-            } else if (resul.length == 0) {
-                response.json({success: false, message: 'El usuario no existe!'});                  
-            }
-        });
-    }
 });
 
 app.listen(PORT, () => {
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -55,66 +55,66 @@ router.post('/registro-usuario', (request, response) => {
 });
 
 
-// // Login usuario
-// /**
-//  * @swagger
-//  * /login-usuario:
-//  *  post:
-//  *    description: Login de usuario de App
-//  *    tags : 
-//  *     - Usuarios
-//  *    requestBody:
-//  *      required: true
-//  *      content:
-//  *        application/json:
-//  *          schema:
-//  *              type: object
-//  *              $ref: '#/components/schemas/Usuario'
-//  *    responses:
-//  *      200:
-//  *        description: Login correcto
-//  *        content: 
-//  *          application/json:
-//  *            schema: 
-//  *              type: array
-//  *              items:
-//  *                $ref : '#/components/schemas/Usuario'
-//  */
-//     router.post('/login-usuario', (request, response) => {
+// Login usuario
+/**
+ * @swagger
+ * /login-usuario:
+ *  post:
+ *    description: Login de usuario de App
+ *    tags : 
+ *     - Usuarios
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *              type: object
+ *              $ref: '#/components/schemas/Usuario'
+ *    responses:
+ *      200:
+ *        description: Login correcto
+ *        content: 
+ *          application/json:
+ *            schema: 
+ *              type: array
+ *              items:
+ *                $ref : '#/components/schemas/Usuario'
+ */
+    router.post('/login-usuario', (request, response) => {
 
-//         const user = {
-//             nombre : request.body.nombre,
-//             password : request.body.password
-//         }
+        const user = {
+            nombre : request.body.nombre,
+            password : request.body.password
+        }
 
-//         if (user.nombre && user.password) {
+        if (user.nombre && user.password) {
 
-//             const sql = `SELECT * FROM usuarios WHERE nombre = '${user.nombre}'`;
+            const sql = `SELECT * FROM usuarios WHERE nombre = '${user.nombre}'`;
 
-//             conexion.query(sql, (error, resul) => {
-//                 if (error) throw error;
-//                 if(resul.length > 0) {
-//                     bcrypt.compare( user.password, resul[0].password, (error, resultado) => {
-//                         if (resultado) {
+            conexion.query(sql, (error, resul) => {
+                if (error) throw error;
+                if(resul.length > 0) {
+                    bcrypt.compare( user.password, resul[0].password, (error, resultado) => {
+                        if (resultado) {
 
-//                             const payLoad = {
-//                                 check : true
-//                             }
+                            const payLoad = {
+                                check : true
+                            }
                     
-//                             const token = jwt.sign(payLoad, router.get('key'), {
-//                                 expiresIn : '7d',
+                            const token = jwt.sign(payLoad, router.get('key'), {
+                                expiresIn : '7d',
                                 
-//                             })
+                            })
 
-//                             response.json({success: true, message: 'Login correcto!', token : token});
-//                             // response.send('Login correcto');
-//                         } else response.json({success: false, message: 'Contraseña incorrecta!'});
-//                     })
-//                 } else if (resul.length == 0) {
-//                     response.json({success: false, message: 'El usuario no existe!'});                  
-//                 }
-//             });
-//         }
-//     });
+                            response.json({success: true, message: 'Login correcto!', token : token});
+                            // response.send('Login correcto');
+                        } else response.json({success: false, message: 'Contraseña incorrecta!'});
+                    })
+                } else if (resul.length == 0) {
+                    response.json({success: false, message: 'El usuario no existe!'});                  
+                }
+            });
+        }
+    });
 
     module.exports = router;
