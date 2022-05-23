9af5cd9aacaa6b0bd6bdcce61fe765a035d339dc
Ivan <ibanpillao@gmail.com>
2022-05-23 11:28:03 +0200

    angular VIII

--- src/index.js
+++ src/index.js
@@ -45,6 +45,68 @@ app.get('/', (request, response) => {
     response.send('<h1>Bizkaiko mendimartxak</h1>');
 });
 
+// Login usuario
+/**
+ * @swagger
+ * /login-usuario:
+ *  post:
+ *    description: Login de usuario de App
+ *    tags : 
+ *     - Usuarios
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *              type: object
+ *              $ref: '#/components/schemas/Usuario'
+ *    responses:
+ *      200:
+ *        description: Login correcto
+ *        content: 
+ *          application/json:
+ *            schema: 
+ *              type: array
+ *              items:
+ *                $ref : '#/components/schemas/Usuario'
+ */
+ app.post('/login-usuario', (request, response) => {
+
+    const user = {
+        nombre : request.body.nombre,
+        password : request.body.password
+    }
+
+    if (user.nombre && user.password) {
+
+        const sql = `SELECT * FROM usuarios WHERE nombre = '${user.nombre}'`;
+
+        conexion.query(sql, (error, resul) => {
+            if (error) throw error;
+            if(resul.length > 0) {
+                bcrypt.compare( user.password, resul[0].password, (error, resultado) => {
+                    if (resultado) {
+
+                        const payLoad = {
+                            check : true
+                        }
+                
+                        const token = jwt.sign(payLoad, router.get('key'), {
+                            expiresIn : '7d',
+                            
+                        })
+
+                        response.json({success: true, message: 'Login correcto!', token : token});
+                        // response.send('Login correcto');
+                    } else response.json({success: false, message: 'Contraseña incorrecta!'});
+                })
+            } else if (resul.length == 0) {
+                response.json({success: false, message: 'El usuario no existe!'});                  
+            }
+        });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor oyendo en el puerto ${PORT}`);
 });
