956fb65663ff3ffdb8ba5592ead0f7a8c9e379c4
Ivan <ibanpillao@gmail.com>
2022-06-08 11:16:15 +0200

    jwt-token III

--- src/settings/verifyKey.js
+++ src/settings/verifyKey.js
@@ -7,10 +7,7 @@ module.exports.verifica = function(req, res, next) {
         req.token = bearerToken;
         next();
     } else {
-        res.sendStatus(403);
-        res.json({
-            error: "Es necesario un token de autentificación"
-        })
+        res.sendStatus(403).send("Es necesario un token de autentificación");
     }
 
 }
